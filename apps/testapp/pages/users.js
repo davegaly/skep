12e13998d66b694ef2025d69a9ffd511cdf31f5c
davegaly <davegaly@gmail.com>
2024-08-20 15:08:40 +0200

    asd

--- apps/testapp/pages/users.js
+++ apps/testapp/pages/users.js
@@ -9,6 +9,15 @@ document.addEventListener('DOMContentLoaded', async function() {
         skepUITableDataBind("tblUsers", usersList);
     }
 
+    // task repeater
+    const rptTasks = document.getElementById('rptTasks');
+    usersList.forEach(item => {
+        const renderedItem = itemTemplate
+          .replace('{{guid}}', item.guid)
+          .replace('{{name}}', item.displayName);
+          rptTasks.innerHTML += renderedItem;
+    });
+
 });
 
 function tblUsersOnEditClick(fieldValue) {
@@ -18,3 +27,10 @@ function tblUsersOnEditClick(fieldValue) {
 function btnCreateOnClick() {
     skepUIGoToPage("usersEdit");
 }
+
+const itemTemplate = `
+  <div class="item">
+    <h3>{{guid}}</h3>
+    <p>{{name}}</p>
+  </div>
+`;
